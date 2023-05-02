9fe0cab959527d32c2d387df7687ee5b20c929ee
Vicious <19979299+Zooney-17@users.noreply.github.com>
2023-05-01 21:24:01 -0400

    Change the display when a number button is clicked

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ let operator;
 function operate(operator, number1, number2) {
     return operator(number1, number2);
 };
+
+//create variables for buttons and display
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.number');
+//on button click, display.textContent = 'whatever button was pressed'
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.addEventListener('click', () => {
+        display.textContent = button.textContent;
+    });
+};
+
+let displayValue = display.textContent;
