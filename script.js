0ce8edba541e8fd849a6f9ef76c743760682a599
Vicious <19979299+Zooney-17@users.noreply.github.com>
2023-05-01 22:25:25 -0400

    Change the display population code

--- script.js
+++ script.js
@@ -22,15 +22,22 @@ function operate(operator, number1, number2) {
     return operator(number1, number2);
 };
 
-//create variables for buttons and display
+let displayValue;
+
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.number');
-//on button click, display.textContent = 'whatever button was pressed'
-for (let i = 0; i < buttons.length; i++) {
-    let button = buttons[i];
+
+// for (let i = 0; i < buttons.length; i++) {
+//     let button = buttons[i];
+//     button.addEventListener('click', () => {
+//         display.textContent = button.textContent;
+//         displayValue = display.textContent;
+//     });
+// };
+
+buttons.forEach((button) => {
     button.addEventListener('click', () => {
         display.textContent = button.textContent;
+        displayValue = display.textContent;
     });
-};
-
-let displayValue = display.textContent;
+});
