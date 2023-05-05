9367cf984f5989cf322396b3e707b47455a8b311
Vicious <19979299+Zooney-17@users.noreply.github.com>
2023-05-05 16:20:10 -0400

    Enable full calculator functionality

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
-const add = function(number1, number2) {
-    return number1 + number2;
-};
+const add = (a, b) => a + b;
 
-const subtract = function(number1, number2) {
-    return number1 - number2;
-};
+const subtract = (a, b) => a - b;
 
-const multiply = function(number1, number2) {
-    return number1 * number2;
-};
+const multiply = (a, b) => a * b;
 
-const divide = function(number1, number2) {
-    return number1 / number2;
-};
+function divide(a, b) {
+    if (b === 0) {
+        return 'yeah right, lmao';
+    } else {
+        return a/b;
+    }
+}
 
 let firstNumber;
 let secondNumber;
 let operator;
-
-function operate(operator, number1, number2) {
-    return operator(number1, number2);
-};
-
 let displayValue;
+let temp;
+
+function operate(operation, firstNumber, secondNumber) {
+    if (operation === '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operation === '-') {
+        return subtract(firstNumber, secondNumber);
+    } else if (operation === '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operation === '÷') {
+        return divide(firstNumber, secondNumber);
+    };
+};
 
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.number');
 
-// for (let i = 0; i < buttons.length; i++) {
-//     let button = buttons[i];
-//     button.addEventListener('click', () => {
-//         display.textContent = button.textContent;
-//         displayValue = display.textContent;
-//     });
-// };
-
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (display.textContent === '0') display.textContent = '';
+        if (firstNumber) {
+            temp = firstNumber;
+            firstNumber = undefined;
+            display.textContent = '';
+        }
         display.textContent += button.textContent;
-        displayValue = display.textContent;
     });
 });
+
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const computations = document.querySelectorAll('.operator');
+
+computations.forEach((computation) => {
+    computation.addEventListener('click', () => {
+        firstNumber = display.textContent;
+        operator = computation.textContent;
+    });
+});
+
+equals.addEventListener('click', () => {
+    secondNumber = display.textContent;
+    firstNumber = temp;
+    display.textContent = `${operate(operator, Number(firstNumber), Number(secondNumber))}`;
+});
+
+clear.addEventListener('click', () => {
+    display.textContent = '0';
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    displayValue = undefined;
+    temp = undefined;
+})
